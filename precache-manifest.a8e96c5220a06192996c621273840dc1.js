0a0b560276642c5e6008e88ebb62b85f5fefa4ea
yasir khan <ykhan5439@gmail.com>
2020-08-05 12:14:30 +0530

    Updates

---
+++ precache-manifest.a8e96c5220a06192996c621273840dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5c1f3e7faa7f6b1da54cf0986cb53e",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "2b4f7cf72681b6231218",
+    "url": "/Monster-Rolodex/static/css/main.71a76e88.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Monster-Rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4f7cf72681b6231218",
+    "url": "/Monster-Rolodex/static/js/main.441433b4.chunk.js"
+  },
+  {
+    "revision": "3e2b7460a7751f8973f3",
+    "url": "/Monster-Rolodex/static/js/runtime-main.1aff1f47.js"
+  }
+]);
